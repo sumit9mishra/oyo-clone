4ad7dbd6b3a28fd87eae232d7588cf6e94d60038
sumit <sumitmishrab@gmail.com>
2021-06-07 15:51:24 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from '../images/logo.svg';
+import {FaAlignRight} from 'react-icons';
+
+
+export default class Navbar extends Component {    
+
+    render() {
+        return (
+            <div>
+                <h1>hello from nav bar
+
+                </h1>
+            </div>
+        )
+    }
+}
